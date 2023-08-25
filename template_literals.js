37e5ffe6968e0a30abf5480cd0966982b9e82050
Kristian Tud <kristiantud@hotmail.com>
2023-08-25 04:27:16 -0600

    finished template literals

---
+++ template_literals.js
@@ -0,0 +1,7 @@
+
+
+const aString = 'this is';
+console.log(`${aString} pretty much the most difficult thing to do` + ` in ` + `template literals`);
+console.log('actually, i \'lied\', this is also a tricky one to note');
+console.log("and perhaps \"this one too\"");
+console.log("but once you get that \\ is an escape character, then you golden, son");
