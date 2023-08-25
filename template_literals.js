7c4e6536c28257e935ac77dda484906dd764119b
Kristian Tud <kristiantud@hotmail.com>
2023-08-25 04:36:06 -0600

    added more template literal stuff

--- template_literals.js
+++ template_literals.js
@@ -5,3 +5,6 @@ console.log(`${aString} pretty much the most difficult thing to do` + ` in ` + `
 console.log('actually, i \'lied\', this is also a tricky one to note');
 console.log("and perhaps \"this one too\"");
 console.log("but once you get that \\ is an escape character, then you golden, son");
+console.log(`so i learned that template literals are more concerned with \` and not \" or even \', but check 
+
+this out, new line without inserting \\n!!`);
