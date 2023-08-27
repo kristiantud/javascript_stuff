d62bbd0b63b2dedeb7003bd0c0bf6552e0af46bc
Kristian Tud <kristiantud@hotmail.com>
2023-08-27 01:14:12 -0600

    finished basic turning functions into arrow functions

--- arrow_functions.js
+++ arrow_functions.js
@@ -24,25 +24,54 @@ console.log(sayHello());
 console.log(sayHello2());
 console.log(sayHello3('Nameless'));
 sayHello4('K', 'T', 28); // Arguments(3) ['K', 'T', 28, .....]
-sayHello5('H','B',27); // Uncaught ReferenceError: arguments is not defined
+// sayHello5('H','B',27); // Uncaught ReferenceError: arguments is not defined
 
-// Callback functions
+// ------------------- //
+
+// Callback functions/higher order functions
 setTimeout(function(){
     return "Hello";
-})
+}, 1000);
+
+// arrow funct
+setTimeout(() => {
+    console.log('Hello!');
+},1000)
+
+
+// ------------------- //
 
 // Named functions
 function sayHelloNamed() {
-    return {};
+    console.log('Hello named');
 }
+sayHelloNamed();
+
+// Arrow function is always anonymous, therefore there is no way to create 
+// arrow function that is also named function
+
+// ------------------- //
 
 // constructor
-function person(n){
+const person = (n) => {
     this.name = n;
 }
 
+// const Justin = new person('Justin'); // Uncaught TypeError: person is not a constructor
+
+// ------------------- //
+
 // Object methods:
 const me = {
-    sayHello: function(){}
+    hello: function(){
+        console.log('Hello object');
+        
+    },
+    hello2: () => { // watch out for this!!
+        console.log("Hello Object2");
+    }
 }
+
+me.hello();
+me.hello2();
 
